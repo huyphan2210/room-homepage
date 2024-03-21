faff30788a30a5edeec8a3d18006b21f8ec81ce9
Huy Phan <huy.phan2210@outlook.com>
2024-03-21 16:14:50 +0100

    Initialize the project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,45 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'prettier',
+    'plugin:prettier/recommended'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  plugins: ['react', 'react-refresh', '@typescript-eslint', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
   },
-}
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off'
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'], // Your TypeScript file extensions
+      rules: {
+        'react/jsx-filename-extension': [2, { extensions: ['.ts', '.tsx'] }], // Allow .ts and .tsx extensions for files containing JSX
+      },
+    },
+  ],
+};
